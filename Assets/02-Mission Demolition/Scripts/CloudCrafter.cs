10010c3136827206fd89224658d23bc1b1b6cd64
NinthFriend <emaullon1@gmail.com>
2024-02-08 20:47:42 -0500

    Game is playable. Need to add few more castle variants.
    Slingshot works. Ball moves and creates projectile line behind it. Added a night background and matched the lighting of it. Might need to tinker around with that more.

--- Assets/02-Mission Demolition/Scripts/CloudCrafter.cs
+++ Assets/02-Mission Demolition/Scripts/CloudCrafter.cs
@@ -41,7 +41,7 @@ public class CloudCrafter : MonoBehaviour
 			cPos.y = Mathf.Lerp(cloudPosMin.y, cPos.y, scaleU);
 
 			// smaller clouds should be farther away
-			cPos.z = 100 -90*scaleU;
+			cPos.z = 100 - 90*scaleU;
 
 			// apply these transforms to the cloud 
 			cloud.transform.position = cPos;
